7a36fa283371b022ed3036554d5255fc3dc3abb8
Manolis Kalafatis <mankal111@hotmail.com>
2018-10-24 17:11:14 +0300

    Updates

---
+++ component---src-pages-index-js-f6df1b3a209fd7b497d6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{131:function(e,n,t){"use strict";t.r(n);var c=t(0),r=t.n(c);n.default=function(){return r.a.createElement("h1",null,"Electromagnetism exercises")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-f6df1b3a209fd7b497d6.js.map
