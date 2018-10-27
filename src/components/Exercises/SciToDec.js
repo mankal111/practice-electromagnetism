07a09e37e70e664e254edc6cc567bd089346412e
Manolis Kalafatis <mankal111@hotmail.com>
2018-10-27 11:44:12 +0300

    corrected random range of scitodec

--- src/components/Exercises/SciToDec.js
+++ src/components/Exercises/SciToDec.js
@@ -16,7 +16,7 @@ export default class SciToDec extends React.Component {
 
     generateValues(event) {
         this.setState({
-            coefficient: JXRand.getNumber({min: 1, max: 1.999})
+            coefficient: JXRand.getNumber({min: 1, max: 9.999})
                 .toFixed(JXRand.getNumber({min: 0, max: 4, type: 'integer'})),
             exponent: JXRand.getNumber({min: -6, max: 6, type: 'integer'})
         });
