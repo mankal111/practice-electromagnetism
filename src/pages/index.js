4b863389dfe95cf4f1d83c4d95cbfb6cd397d785
Manolis Kalafatis <mankal111@hotmail.com>
2018-10-27 11:44:31 +0300

    styled index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import SciToDec from "../components/Exercises/SciToDec"
 import DecToSci from "../components/Exercises/DecToSci"
+import indexStyles from "./index.module.css"
 
 export default () => (
-    <div>
+    <div className={indexStyles.main}>
         <h1>Electromagnetism exercises</h1>
         <SciToDec />
         {/* <DecToSci /> */}
