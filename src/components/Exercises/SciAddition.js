2b33ac3eded8f84bd1c36687b528a49571999a25
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-06 11:24:41 +0200

    sciAddition answer check

--- src/components/Exercises/SciAddition.js
+++ src/components/Exercises/SciAddition.js
@@ -42,15 +42,21 @@ export default class SciAddition extends React.Component {
     render() {
         const { coefficient: coefficient1, exponent: exponent1 } = this.state.number1;
         const { coefficient: coefficient2, exponent: exponent2 } = this.state.number2;
-        console.log(this.state)
+        const number1 = math.eval(`${coefficient1}*10^${exponent1}`);
+        const number2 = math.eval(`${coefficient2}*10^${exponent2}`);
+        const sum = Number(number1) + Number(number2);
+        const sumInScientific = {};
+        ({0: sumInScientific.coefficient, 1: sumInScientific.exponent} =
+            sum.toExponential().split('e').map(item => Number(item)));
+
         return <Exercise 
             title="Scientific notation addition"
             description="Practice on adding two numbers in scientific notation"
             question={`${coefficient1}\\times10^{${exponent1}}+${coefficient2}\\times10^{${exponent2}}=`}
             answer={[{
                 type: "scientific-notation",
-                coefficient: coefficient1,
-                exponent: exponent1
+                coefficient: sumInScientific.coefficient,
+                exponent: sumInScientific.exponent
             }]}
             solution={[]}//this.generateSolutionArray()}
             generateNewValues={this.generateValues}
