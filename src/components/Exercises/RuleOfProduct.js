9dd714605caa3eb5fd93ecdcd4a248228105f4c4
Manolis Kalafatis <mankal111@hotmail.com>
2019-02-09 20:54:50 +0200

    created a value generator for the theater excercise

--- src/components/Exercises/RuleOfProduct.js
+++ src/components/Exercises/RuleOfProduct.js
@@ -22,17 +22,38 @@ export default class RuleOfProduct extends React.Component {
                 questionId: 'theater',
                 question: [
                     "The seats of a theater are numbered by the letter A, B or C,",
-                    "followed by 2 digits, how many seats can we number that way?",
+                    "followed by 2 digits. How many seats can we number that way?",
                 ],
-                answer: 300,
+                randomVariables: [
+                    { name: 'rangeOfLetters', range: [3, 6] },
+                    { name: 'numberOfLetters', range: [1, 3] },
+                    { name: 'numberOfDigits', range: [1, 5] },
+                ],
+                answer: (rangeOfLetters, numberOfLetters, numberOfDigits) => {
+                    return (Math.pow(rangeOfLetters, numberOfLetters) * Math.pow(10, numberOfDigits));
+                }
+            },
+            {
+                questionId: 'theater2',
+                question: [
+                    "The seats of a theater are numbered by the letter A, B or C,",
+                    "followed by 3 digits. How many seats can we number that way?",
+                ],
+                answer: 3000,
             },
         ]
 
-        var newQuestion;
-        do {
-            newQuestion = JXRand.getRandomElement(questionList);
-        } while (newQuestion.questionId === this.state.questionId);
-        this.setState(newQuestion);
+        // var newQuestion;
+        // do {
+        //     newQuestion = JXRand.getRandomElement(questionList);
+        // } while (newQuestion.questionId === this.state.questionId);
+        const randomVariables = questionList[0].randomVariables.map((item) => JXRand.getNumber({min: item.range[0], max: item.range[1], type: 'integer'}));
+        const renderedQuestion = [
+            `The seats of a theater are numbered by ${randomVariables[1] === 1 ? 'a letter' : randomVariables[1] + ' letters'} (A-${String.fromCharCode(64+randomVariables[0])}),`,
+            `followed by ${randomVariables[2]} digits. How many seats can we number that way?`,
+        ];
+
+        this.setState({question: renderedQuestion, answer: questionList[0].answer(randomVariables[0], randomVariables[1], randomVariables[2])});
     }
 
     componentDidMount() {
